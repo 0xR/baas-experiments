9bdb6ac37713fd550ce487991152f9bb696cf288
Ruben Oostinga <ruben.oostinga@gmail.com>
2018-09-21 12:19:07 +0200

    Add initial query

--- src/App.js
+++ src/App.js
@@ -1,21 +1,51 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Amplify, { graphqlOperation }  from "aws-amplify";
+import { Connect } from "aws-amplify-react";
+import awsExports from './aws-exports';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+Amplify.configure(awsExports);
+
+const ListSessions = `  query ListSessions(
+    $filter: ModelSessionFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listSessions(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        startsAt
+        name
+        description
+      }
+      nextToken
+    }
   }
+`;
+
+class App extends React.Component {
+
+    render() {
+
+        const ListView = ({ events }) => (
+            <div>
+                <h3>All events</h3>
+                <ul>
+                    {events && events.length && events.map(event => <li key={event.id}>{event.name} ({event.id})</li>)}
+                </ul>
+            </div>
+        );
+
+        console.log({
+            ListSessions
+        });
+        return (
+            <Connect query={graphqlOperation(ListSessions)}>
+                {(...args) => (
+                    <pre>{JSON.stringify(args, null, 2)}</pre>
+                )}
+            </Connect>
+        )
+    }
 }
 
 export default App;
